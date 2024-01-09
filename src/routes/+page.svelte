10afb6e195a461c80bbc8690b736d2c509b9b16c
marc101101 <markus.guder@gmail.com>
2024-01-09 15:58:31 +0100

    add build path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
 <script>
   import "../app.css";
+  let pace = "6:30";
+  let km = 0;
+  let time = splitPace() * km;
+  let timeHour = time / 60;
+  let speedKMH = 0;
+
+  function splitPace() {
+    const [min, sec] = pace.split(":");
+    return parseInt(min) + parseInt(sec) / 60;
+  }
+
+  function setTimes() {
+    time = splitPace() * km;
+    timeHour = Math.round((time / 60) * 100) / 100;
+  }
+
+  function setSpeed() {
+    speedKMH = Math.round((60 / splitPace()) * 100) / 100;
+  }
+
+  setSpeed();
 </script>
 
 <div class="h-screen flex items-center justify-center">
@@ -17,13 +38,14 @@
           class="block text-gray-700 text-sm font-bold mb-2"
           for="username"
         >
-          Pace (1km/min)
+          ⌀ Kilometerzeit / Pace (min)
         </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="paste"
-          type="text"
-          placeholder="Durschnittliche Kilometerzeit"
+          bind:value={pace}
+          on:input={() => (setTimes(), setSpeed())}
+          type="float"
+          placeholder="Pace (1km/min)"
         />
       </div>
       <div class="mb-4">
@@ -31,29 +53,80 @@
           class="block text-gray-700 text-sm font-bold mb-2"
           for="username"
         >
-          Kilometer
+          ⌀ Geschw. (km/h)
         </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="paste"
-          type="text"
+          bind:value={speedKMH}
+          on:input={() => setTimes()}
+          type="float"
+          placeholder="Pace (1km/min)"
+        />
+      </div>
+      <hr />
+      <div class="mb-4">
+        <label
+          class="block text-gray-700 text-sm font-bold mb-2"
+          for="username"
+        >
+          Strecke (km)
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          bind:value={km}
+          on:input={() => setTimes()}
+          type="float"
           placeholder="Kilometer"
         />
       </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="password"
-        >
-          Gesamt Zeit
-        </label>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 text-xs rounded"
+        on:click={() => ((km = 21), setTimes())}
+      >
+        Halbmarathon (21km)
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 text-xs rounded"
+        on:click={() => ((km = 42), setTimes())}
+      >
+        Marathon (42km)
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 text-xs rounded mt-1"
+        on:click={() => ((km = 10.5), setTimes())}
+      >
+        Viertel­marathon (10,5km)
+      </button>
 
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="paste"
-          type="text"
-          placeholder="Deine Gesamt Zeit"
-        />
+      <div class="flex flex-row mt-3">
+        <div class="mb-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Zeit (min:sec)
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            bind:value={time}
+            type="text"
+            placeholder="Deine Gesamt Zeit"
+          />
+        </div>
+        <div class="mb-6 ml-6">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Zeit (h:min)
+          </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            bind:value={timeHour}
+            type="text"
+            placeholder="Deine Gesamt Zeit"
+          />
+        </div>
       </div>
     </form>
     <a href="https://guder-service.com/impressum">
